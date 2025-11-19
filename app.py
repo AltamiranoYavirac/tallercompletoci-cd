4db9db9a112c0fe00de0be98c1da25d173c2ed28
Jossy2005 <jab.mena@yavirac.edu.ec>
2025-11-19 10:24:01 -0500

    Archivos configurados

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+
 def home():
     return "<h1>Hola desde Flask con Traefik 🚀</h1>"
 
+
 @app.route('/saludo/<nombre>')
 def saludo(nombre):
+    
+    
     return f"<h2>Hola {nombre}, bienvenido a pgmoreno.byronrm.com</h2>"
 
 if __name__ == '__main__':
